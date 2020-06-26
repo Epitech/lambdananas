6a2897102dfa87faafca1f9d93f5985cd6fed7ef
Marc Planard <marc.planard@epitech.eu>
2020-06-26 15:02:27 +0200

    Add usage and options

--- app/Main.hs
+++ app/Main.hs
@@ -6,18 +6,62 @@ import Data.Monoid
 import Control.Monad
 import Data.List
 
-rules :: [Check]
-rules = [ checkSigs, checkIfs, checkReturns, checkDos, checkGuards ]
+data Rule = Rule { name :: String
+                 , description :: String
+                 , getRule :: Check
+                 }
 
-doOne :: String -> IO ()
-doOne filename = do
-  --putStrLn $ "------ " ++ filename ++ ":"
+instance Eq Rule where
+  r1 == r2 = name r1 == name r2
+
+allRules :: [Rule]
+allRules = [
+  Rule "check-signatures"
+  "top declaration has no corresponging type signature"
+  checkSigs,
+  Rule "check-ifs"
+  "nested if"
+  checkIfs,
+  Rule "check-returns"
+  "useless return statement in do block"
+  checkReturns,
+  Rule "check-dos"
+  "useless do"
+  checkDos,
+  Rule "check-guards"
+  "guard should be pattern match"
+  checkGuards
+  ]
+
+rulesLook :: [Rule] -> [(String,Rule)]
+rulesLook = map (\ r -> (name r, r))
+
+doOne :: [Rule] -> String -> IO ()
+doOne rules filename = do
   buff <- parseFile filename
   case buff of
-    Right lst -> let warnings = sort $ join $ map (\ f -> f lst) rules
+    Right lst -> let rs = map getRule rules
+                     warnings = sort $ join $ map (\ f -> f lst) rs
                  in mapM_ print warnings
     Left err -> putStrLn $ "unable to load file: "++ show (err :: IOError)
-    
-main :: IO ()
-main = getArgs >>= mapM_ doOne
 
+usage :: IO ()
+usage = putStrLn "usage: hsc [--disable rule] [--enable rule] [files]"
+        >> putStrLn "  - Rules:"
+        >> mapM_ displayRule allRules
+  where displayRule (Rule name description _) =
+          putStrLn ("    * "++ name ++":\n      "++description)
+
+doArgs :: [Rule] -> [String] -> Maybe ([Rule],[String])
+doArgs rules [] = Nothing
+doArgs rules ("--disable":y:xs)
+  | y `elem` map name rules = doArgs [r | r <- rules, name r /= y ] xs
+  | otherwise = Nothing
+doArgs rules ("--enable":y:xs) = lookup y (rulesLook allRules) >>=
+  \ r -> doArgs (if rules == allRules then [r] else rules++[r]) xs
+doArgs rules lst = Just (rules, lst)
+        
+main :: IO ()
+main = getArgs >>= processAll . doArgs allRules
+  where processAll Nothing = usage
+        processAll (Just (rules, files)) = mapM_ (doOne rules) files

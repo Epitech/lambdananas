78193adc8dd9d41e30784cd162323bd60a9ca4d9
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-16 15:52:38 +0200

    Add negative extension test

--- test/system/examples/ForbiddenExtension.hs
+++ test/system/examples/ForbiddenExtension.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NPlusKPatterns #-}
+{-# LANGUAGE GADTs #-}
 
 module Main where
 
@@ -7,6 +10,23 @@ import Text.Read
 import Data.Maybe
 
 data BTree a = Leaf a | Trunk (BTree a) (BTree a) deriving Foldable
+
+fun :: Int -> String
+fun = \ n -> case n of {1 -> "un"; 2 -> "deux"; 3 -> "trois"; _ -> "yoyele"}
+
+data Term a where
+    Lit    :: Int -> Term Int
+    Succ   :: Term Int -> Term Int
+    IsZero :: Term Int -> Term Bool
+    If     :: Term Bool -> Term a -> Term a -> Term a
+    Pair   :: Term a -> Term b -> Term (a,b)
+
+eval :: Term a -> a
+eval (Lit i)      = i
+eval (Succ t)     = 1 + eval t
+eval (IsZero t)   = eval t == 0
+eval (If b e1 e2) = if eval b then eval e1 else eval e2
+eval (Pair e1 e2) = (eval e1, eval e2)
 
 createSimpleBTree :: BTree Int
 createSimpleBTree = Trunk (Leaf 1) (Trunk (Leaf 2) (Leaf 4))

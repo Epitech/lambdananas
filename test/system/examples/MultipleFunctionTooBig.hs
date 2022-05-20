8dfe94a080b270ef1b064a29ec1549782c65198e
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-20 11:04:25 +0200

    Add basic vague output system test

---
+++ test/system/examples/MultipleFunctionTooBig.hs
@@ -0,0 +1,43 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "Marc est en vacances" >> putStrLn "mais c'est pas grave" >> putStrLn "je fais genre je travaille" >> fun
+
+fun :: IO ()
+fun = putStrLn "c'est faux" >>
+  putStrLn "stp" >>
+  putStrLn "me tues pas" >>
+  putStrLn "donne juste du" >>
+  putStrLn "télétravaille" >>
+  putStrLn "avant que je pleure" >>
+  putStrLn "si t'es sympa" >>
+  putStrLn "aide moi à devenir ape" >>
+  putStrLn "en tek5" >>
+  putStrLn "jte jure je serai" >>
+  putStrLn "moins con"
+
+fun' :: IO ()
+fun' = putStrLn "c'est faux" >>
+  putStrLn "stp" >>
+  putStrLn "me tues pas" >>
+  putStrLn "donne juste du" >>
+  putStrLn "télétravaille" >>
+  putStrLn "avant que je pleure" >>
+  putStrLn "si t'es sympa" >>
+  putStrLn "aide moi à devenir ape" >>
+  putStrLn "en tek5" >>
+  putStrLn "jte jure je serai" >>
+  putStrLn "moins con"
+
+fun'' :: IO ()
+fun'' = putStrLn "c'est faux" >>
+  putStrLn "stp" >>
+  putStrLn "me tues pas" >>
+  putStrLn "donne juste du" >>
+  putStrLn "télétravaille" >>
+  putStrLn "avant que je pleure" >>
+  putStrLn "si t'es sympa" >>
+  putStrLn "aide moi à devenir ape" >>
+  putStrLn "en tek5" >>
+  putStrLn "jte jure je serai" >>
+  putStrLn "moins con"

a23db86fc7583515f6d039132766673996891f13
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-24 16:12:42 +0200

    Add splitting excluded directories string

--- src/Input.hs
+++ src/Input.hs
@@ -9,26 +9,29 @@ import System.Directory
 import System.FilePath.Posix
 import Control.Monad
 
+type ManualExclusions = [String]
+
 -- | A list of directories to be ignored.
-ignoredDirs :: [String]
-ignoredDirs = ["tests", "test", "bonus", ".stack-work", ".git"]
+defaultExclusions :: [String]
+defaultExclusions = ["tests", "test", "bonus", ".stack-work", ".git"]
 
 -- | If the given 'FilePath' is a directory, returns a list
 -- of all files inside recursively. Given a file, will
 -- return it as a single element list.
-load :: FilePath -> IO [FilePath]
-load f = doesDirectoryExist f >>=
+load :: ManualExclusions -> FilePath -> IO [FilePath]
+load excluded f = doesDirectoryExist f >>=
                     \ isDir -> if isDir && ignore f
-                               then loadDir f
+                               then loadDir excluded f
                                else return [f]
-  where ignore fl = takeFileName fl `notElem` ignoredDirs
+  where ignore fl = takeFileName fl `notElem` defaultExclusions <> excluded
 
 -- | Given a 'FilePath' returns all haskell files found recursively.
 -- example: `File.hs` gives `[File.hs]`
 -- example: `src/` could give `[src/File1.hs, src/File2.hs]`
-loadDir :: FilePath -> IO [FilePath]
-loadDir dir = do
+loadDir :: ManualExclusions -> FilePath -> IO [FilePath]
+loadDir excluded dir = do
     files <- listDirectory dir
-    files2 <- mapM (load . (dir </>)) files
+    files2 <- mapM (load excluded . (dir </>)) files
     return $ filter (\ f -> takeExtension f == ".hs" &&
                           takeFileName f /= "Setup.hs") $ join files2
+

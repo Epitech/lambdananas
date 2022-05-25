f5b633d5f2f10c0561c5bdf7fae5c2ab26bc6c00
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-25 17:50:11 +0200

    Fix build errors

--- src/Output.hs
+++ src/Output.hs
@@ -5,19 +5,19 @@ module Output (
   outputOne,
   outputOneErr,
   outputVague,
-  argoOutputFiles,
-  dumpManifest,
+  outputManifest,
   module Parser,
   module Rules,
+  module Warn,
 ) where
-
-import Data.Maybe
-import Data.List
 
 import Conf
 import Rules
 import Parser
 import Warn
+
+import Data.Maybe
+import Data.List
 
 -- | Lookup table of gravities linked to their path.
 -- Used in argos mode only.
@@ -34,12 +34,13 @@ createArgoFileName s = "style-" <> s <> ".txt"
 -- | Output the result of a single coding style issue.
 outputOne :: Conf -> Warn -> IO ()
 outputOne Conf {mode = Just Silent} _ = return ()
-outputOne Conf {mode = Just Argos} w@(Warn _ _ g) =
-    appendFile atPath $ show (Argos, w) <> "\n"
+outputOne Conf {mode = Just Argos} w@Warn {issue = i} =
+    appendFile atPath $ showArgos w <> "\n"
   where
-    atPath = fromMaybe errorsPath $ lookup g argoOutputFiles
+    atPath = fromMaybe errorsPath $ lookup (getGravity i) argoOutputFiles
+    getGravity a = case lookupIssueInfo a of IssueInfo {gravity=g} -> g
     errorsPath = createArgoFileName "debug"
-outputOne _ w = putStrLn $ show (Vera, w)
+outputOne _ w = putStrLn $ showVera w
 
 -- TODO : this is very much temporary and should be patched when
 -- switching backend parsing library.
@@ -49,28 +50,28 @@ outputOneErr Conf {mode = Just Silent} _ =
   return ()
 outputOneErr Conf {mode = Just Argos} (ParseError filename loc _ text)
     | "Parse error:" `isPrefixOf` text = appendFile atPath $
-      showArgo notParsableIssue <> "\n"
+      showArgos notParsableIssue <> "\n"
     -- everything not a parse error is an extension error
     | otherwise = appendFile "banned_funcs" $
-      showArgo forbiddenExtIssue <> "\n"
+      showArgos forbiddenExtIssue <> "\n"
   where
     atPath = fromMaybe errorsPath $ lookup Major argoOutputFiles
     errorsPath = createArgoFileName "debug"
-    notParsableIssue = Warn (NotParsable filename) (filename, loc) Major
-    forbiddenExtIssue = Warn (ForbiddenExt filename) (filename, loc) Major
+    notParsableIssue = makeWarn NotParsable (filename, loc) $ StringArg filename
+    forbiddenExtIssue = makeWarn ForbiddenExt (filename, loc) $ StringArg filename
 outputOneErr _ (ParseError filename loc _ text)
     | "Parse error:" `isPrefixOf` text = putStrLn $ showVera issue
     -- everything not a parse error is an extension error
-    | otherwise = putStrLn $ snd $ getIssueDesc $ ForbiddenExt filename
+    | otherwise = putStrLn $ showDetails (lookupIssueInfo ForbiddenExt) (StringArg filename)
   where
-    issue = Warn (NotParsable filename) (filename, loc) Major
+    issue = makeWarn NotParsable (filename, loc) $ StringArg filename
 
 -- | Dumps a manifest of all coding style issues in format
 -- `<code>:<description>`.
-dumpManifest :: String
-dumpManifest = foldr mergeLines "" (merge <$> getIssuesList)
+outputManifest :: String
+outputManifest = foldr mergeLines "" (merge <$> issues)
   where
-    merge (a, b) = a ++ ':':b
+    merge (_, IssueInfo {code = c, showDetails = d}) = c ++ ':':d NoArg
     mergeLines e acc = e ++ '\n':acc
 
 -- | Appends a vague description of issues to 'style-student.txt'.
@@ -81,7 +82,7 @@ outputVague Conf{mode=Just Argos} i = appendFile (createArgoFileName "student") 
     accumulate :: [Issue] -> [(Issue, Int)]
     accumulate = foldr accumulateVague []
     toString :: [(Issue, Int)] -> String
-    toString l = concat $ ( \ (issue, x) -> showVague issue x) <$> l
+    toString l = concat $ ( \ (i', x) -> showVague i' x) <$> l
 outputVague _ _ = return ()
 
 accumulateVague :: Issue -> [(Issue, Int)] -> [(Issue, Int)]
@@ -89,12 +90,33 @@ accumulateVague i acc = case lookup i acc of
                           Nothing -> (i, 1):acc
                           Just n -> (i, n + 1):delete (i, n) acc
 
+showArgos :: Warn -> String
+showArgos w@Warn {issue = i} =
+    filename ++ ':':show issueLine ++ ':':issueCode
+  where
+    info = lookupIssueInfo i
+    issueCode = code info
+    issueLine = snd $ loc w
+    filename = fst $ loc w
+
+showVera :: Warn -> String
+showVera w@Warn {issue = i, arg = a} =
+    filename ++ ':':issueLine ++ ':':issueGravity ++ ':':' ':issueCode ++
+    " # " ++ issueDesc
+  where
+    info = lookupIssueInfo i
+    issueDesc = showDetails info a
+    issueCode = code info
+    issueLine = show $ snd $ loc w
+    issueGravity = show $ gravity info
+    filename = fst $ loc w
+
 -- | Creates a vague description of a given issue.
 showVague :: Issue    -- ^ issue
           -> Int      -- ^ number of times it was raised
           -> String
-showVague issue n =
-  code ++ " rule has been violated " ++ show n ++ " times: " ++ des ++ "\n"
+showVague i n =
+  issueCode ++ " rule has been violated " ++ show n ++ " times: " ++ details ++ "\n"
   where
-    code = fst $ getIssueDesc issue
-    des = snd $ getIssueDesc issue
+    issueCode = code $ lookupIssueInfo i
+    details = (showDetails $ lookupIssueInfo i) NoArg

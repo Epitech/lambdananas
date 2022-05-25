7636c6c969dff5a924005f5a509bc8f9b88741b7
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-25 14:33:26 +0200

    Add smart constructors and IssueArgs
    Goal here is to avoid misuse of warns.
    (making a warn with an issue not matching the args)
    IssueArgs are introduced to replace and be a cleaner
    version of meta issue datas.

--- src/Warn.hs
+++ src/Warn.hs
@@ -3,28 +3,32 @@ Warnings that can be emitted by the rules checker functions.
 -}
 module Warn (
   Issue (..),
-  Warn (..),
+  Warn (issue, loc, arg),
+  makeWarn,
   Gravity (..),
   issues,
 ) where
 
 -- | A coding style warning that can be emitted by a rule's check function.
-data Warn = Warn { what :: Issue            -- ^ The issue raised
+data Warn = Warn { issue :: Issue           -- ^ The issue raised
                  , loc :: (FilePath, Int)   -- ^ The location of the issue
-                 , meta :: Maybe [String]   -- ^ Metadata to pass to the output 'String'
+                 , arg :: IssueArg          -- ^ Arguments to pass to the output functions
                  } deriving Eq
 
 instance Ord Warn where
   compare (Warn _ (s1,l1) _) (Warn _ (s2,l2) _) | s1 == s2 = compare l1 l2
                                             | otherwise = compare s1 s2
+-- | 'Warn' smart constructor.
+-- Checks that there is no wrong Issue / Arg combination.
+-- See https://wiki.haskell.org/Smart_constructors
+makeWarn :: Issue -> (FilePath, Int) -> IssueArg -> Warn
+makeWarn i@NoSig l a@(StringArg _) = Warn i l a
+makeWarn _ _ _= error "invalid Issue/Arg combination"
 
--- | Describes an 'Issue' gravity.
-data Gravity = Info | Minor | Major deriving Eq
-
-instance Show Gravity where
-  show Info = "INFO"
-  show Minor = "MINOR"
-  show Major = "MAJOR"
+-- | 'Warn' dumb constructor. Disables all checks.
+-- Please use 'makeWarn' instead of this.
+unsafeMakeWarn :: Issue -> (FilePath, Int) -> IssueArg -> Warn
+unsafeMakeWarn i l a = Warn i l a
 
 -- | Enumeration of all possible style issues arising from a program.
 data Issue = BadIf                -- ^ Nested ifs
@@ -39,17 +43,17 @@ data Issue = BadIf                -- ^ Nested ifs
            | Debug                -- ^ Debug
            deriving Eq
 
--- | Data linked to every issue
-data IssueData = IssueData { gravity :: Gravity                 -- ^ Gravity of the issue
-                           , code :: String                     -- ^ The code of the issue
-                           , desc :: Maybe [String] -> String   -- ^ A function returning a short description and taking metadatas
-                           , hint :: String }                   -- ^ A hint about how the student can solve the issue
 
-type Code = String
-type Hint = String
-type Description = Maybe [String] -> String
+-- | Data linked to every issue.
+data IssueInfo = IssueInfo { gravity :: Gravity               -- ^ Gravity of the issue
+                           , code :: String                   -- ^ The code of the issue
+                           , desc :: IssueArg -> String       -- ^ A function returning a short description and taking metadatas
+                           , hint :: String }                 -- ^ A hint about how the student can solve the issue
 
-issues :: [(Issue, IssueData)]
+-- | Arguments to be given to an issue.
+data IssueArg = NoArg | StringArg String deriving (Show, Eq)
+
+issues :: [(Issue, IssueInfo)]
 issues = [(BadIf, dataBadIf),
           (BadDo, dataBadDo),
           (BadReturn, dataBadReturn),
@@ -61,84 +65,92 @@ issues = [(BadIf, dataBadIf),
           (ForbiddenExt, dataForbiddenExt),
           (Debug, dataDebug)]
 
-dataBadIf :: IssueData
-dataBadIf = IssueData
+dataBadIf :: IssueInfo
+dataBadIf = IssueInfo
   Major
   "C1"
   (const "nested if")
   "ifs constructs should not be nested"
 
-dataBadDo :: IssueData
-dataBadDo = IssueData
+dataBadDo :: IssueInfo
+dataBadDo = IssueInfo
   Major
   "D1"
   (const "useless do")
   "do structures should be avoided when no generator are used"
 
-dataBadReturn :: IssueData
-dataBadReturn = IssueData
+dataBadReturn :: IssueInfo
+dataBadReturn = IssueInfo
   Minor
   "D2"
   (const "useless generator")
   "do structures should not have useless return statements"
 
-dataBadGuard :: IssueData
-dataBadGuard = IssueData
+dataBadGuard :: IssueInfo
+dataBadGuard = IssueInfo
   Major
   "C2"
   (const "guard should be a pattern")
   "guard constructs should only be used if it cannot be a pattern match"
 
-dataLineTooLong :: IssueData
-dataLineTooLong = IssueData
+dataLineTooLong :: IssueInfo
+dataLineTooLong = IssueInfo
   Minor
   "F3"
   (const "line too long")
   "lines should be less than 80 columns wide"
 
-dataFunctionTooBig :: IssueData
-dataFunctionTooBig = IssueData
+dataFunctionTooBig :: IssueInfo
+dataFunctionTooBig = IssueInfo
   Minor
   "F4"
   (const "function too big")
   "every functions definitions should be less than 10 lines"
 
-dataNoSig :: IssueData
-dataNoSig = IssueData
+dataNoSig :: IssueInfo
+dataNoSig = IssueInfo
   Minor
   "T1"
   description
   "top-level declaration has no corresponging type signature"
   where
-    description (Just (s:_)) = s ++ " has no signature"
+    description (StringArg s) = s ++ " has no signature"
     description _ = "function has no signature"
 
-dataForbiddenExt :: IssueData
-dataForbiddenExt = IssueData
+dataForbiddenExt :: IssueInfo
+dataForbiddenExt = IssueInfo
   Major
   "E1"
   description
   "line too long"
   where
-    description (Just (s:_)) = s ++ " contains forbidden extensions"
+    description (StringArg s) = s ++ " contains forbidden extensions"
     description _ = "a file contains forbidden extensions"
 
-dataNotParsable :: IssueData
-dataNotParsable = IssueData
+dataNotParsable :: IssueInfo
+dataNotParsable = IssueInfo
   Major
   "P1"
   description
   "all files must be parsable without extensions"
   where
-    description (Just (s:_)) = s ++ " could not be parsed"
+    description (StringArg s) = s ++ " could not be parsed"
     description _ = "a file could not be parsed"
 
-dataDebug :: IssueData
-dataDebug = IssueData
+dataDebug :: IssueInfo
+dataDebug = IssueInfo
   Info
   "XX"
   description
   "debug rule found something, this should not happen!"
   where
-    description (Just (s:_)) = s
+    description (StringArg s) = s
     description _ = "debug rule left empty"
+
+-- | Describes an 'Issue' gravity.
+data Gravity = Info | Minor | Major deriving Eq
+
+instance Show Gravity where
+  show Info = "INFO"
+  show Minor = "MINOR"
+  show Major = "MAJOR"

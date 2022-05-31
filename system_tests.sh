e1d73453a683a216118b53e3e7312323288672ab
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-31 16:29:50 +0200

    Update script to install the executable
    Trying to avoid stack run to not cause the stack logs
    inside system tests bug.

--- system_tests.sh
+++ system_tests.sh
@@ -5,10 +5,15 @@ exit_status=1
 if command -v shelltest
 then
     echo "shelltest will run on all .test files of the test/ directory"
-    stack build 2> /dev/null
+    stack install --local-bin-path .
     shelltest test/
     exit_status=$?
-    rm -f style-minor.txt style-major.txt style-info.txt style-student.txt banned_funcs > /dev/null
+    rm -f style-minor.txt \
+      style-major.txt \
+      style-info.txt \
+      style-student.txt \
+      haskell-style-checker-exe \
+      banned_funcs > /dev/null
     exit $exit_status
 else
     echo "shelltestrunner could not be found"

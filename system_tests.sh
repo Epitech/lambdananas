0a952d0231aab180bff499f071696b4b86e114a6
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-31 14:46:40 +0200

    Update system_tests.sh
    Trying to do some things with the conditionals.

--- system_tests.sh
+++ system_tests.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-exit_status=0
+exit_status=1
 
-if ! command -v shelltest &> /dev/null
+if command -v shelltest
 then
-    echo "shelltestrunner could not be found"
-    echo "make sure the executable is in your PATH"
-    echo "you can download it using 'stack install shelltestrunner-1.9'"
-    exit 1
-else
     echo "shelltest will run on all .test files of the test/ directory"
     stack build --exec "shelltest test/"
     exit_status=$?
     rm -f style-minor.txt style-major.txt style-info.txt style-student.txt banned_funcs > /dev/null
+    exit $exit_status
+else
+    echo "shelltestrunner could not be found"
+    echo "make sure the executable is in your PATH"
+    echo "you can download it using 'stack install shelltestrunner-1.9'"
     exit $exit_status
 fi

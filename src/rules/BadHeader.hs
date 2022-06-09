8800f65467caf2494d87b7bfa8dee2c981c1fea9
Arthur Soulié <arthur.soulie@tuta.io>
2022-06-09 11:46:43 +0200

    Add regex to check the Epitech headers
    Choosed regex-tdfa because it seemed to have high performance
    and lazyness but i am no expert, feel free to replace that!

--- src/rules/BadHeader.hs
+++ src/rules/BadHeader.hs
@@ -6,11 +6,15 @@ module BadHeader (
 ) where
 
 import Common
+import Text.Regex.TDFA
 
 check :: Check
 check (_, comments, f) = case filter isHeader comments of
     [_] -> []
     _ -> [makeWarn BadHeader (f, 1) (StringArg f)]
   where
-    isHeader (Comment True (SrcSpan _ 1 1 _ _) _) = True
+    isHeader (Comment True (SrcSpan _ 1 1 _ _) s)
+      | s =~ regex = True
+      | otherwise = False
     isHeader _ = False
+    regex = "-- EPITECH PROJECT, [0-9]+\n--.*\n-- File description:\n-- .*$"

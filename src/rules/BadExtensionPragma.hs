2ac8e08378150055a4d01d1e92f7e48997f0507d
Arthur Soulié <arthur.soulie@tuta.io>
2022-06-24 23:50:06 +0200

    Fix build error (wip)

--- src/rules/BadExtensionPragma.hs
+++ src/rules/BadExtensionPragma.hs
@@ -9,6 +9,8 @@ import Common
 
 import GHC.LanguageExtensions.Type
 import Data.List
+import Data.Maybe
+import Text.Regex.TDFA
 
 -- | Extensions to authorize.
 -- Any other extensions will generate a 'BadExtensionPragma' warning.
@@ -21,7 +23,7 @@ check :: ParseSuccess -> [Warn]
 check m = genWarn =<< (extensions <$> comments m)
   where
     extensions :: Located AnnotationComment -> Located (Maybe [Extension])
-    extensions a = languagePragmas $ blocks <$> a
+    extensions a = languagePragma $ blocks <$> a
     blocks :: AnnotationComment -> Maybe String
     blocks (AnnBlockComment s) = Just s
     blocks _ = Nothing
@@ -45,10 +47,20 @@ unauthorizedExtension z@(L _ (Just [e]))
     []
 unauthorizedExtension _ = []
 
--- TODO : Add the regex based match
-languagePragmas :: Located (Maybe String) -> Located (Maybe [Extension])
-languagePragmas s = parse <$> s
+languagePragma :: Located (Maybe String) -> Located (Maybe [Extension])
+languagePragma s = parse <$> s
   where
     parse :: Maybe String -> Maybe [Extension]
-    parse (Just _) = Just [GADTs]
-    parse Nothing = Nothing
+    parse a = fromMaybe Nothing (foldr fun (Just []) <$> (parsePragma <$> a))
+    fun :: Maybe Extension -> Maybe [Extension] -> Maybe [Extension]
+    fun (Just e) l = (e:) <$> l
+    fun Nothing Nothing = Nothing
+    fun Nothing _ = Nothing
+
+parsePragma :: String -> [Maybe Extension]
+parsePragma s
+  | s =~ "{-# *LANGUAGE (([A-Za-z])+ *,? *)+ *#-}" = toExt <$> (getAllTextMatches (s =~ "[a-zA-Z]+") :: [String])
+  | otherwise = []
+  where
+    toExt :: String -> Maybe Extension
+    toExt e = e `elemIndex` (show <$> enumFrom Cpp) >>= (\z -> Just $ toEnum z)

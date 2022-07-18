845358dde29ca24b9c7dfbc1c1ffeb3f8d0eccfa
Arthur Soulié <arthur.soulie@tuta.io>
2022-07-18 10:25:24 +0200

    Last commit adding unfinished wip code

--- src/Rules.hs
+++ src/Rules.hs
@@ -11,7 +11,6 @@ import BadExtensionPragma
 import BadGuard
 import BadHeader
 import BadIf
-import FunctionTooWideOrLarge
 import NoExportDecl
 import NoModuleDecl
 import NoSig
@@ -24,7 +23,6 @@ defaultRules = [ BadDo.check
                , BadGuard.check
                , BadHeader.check
                , BadIf.check
-               , FunctionTooWideOrLarge.check
                , NoExportDecl.check
                , NoModuleDecl.check
                , NoSig.check ]
--- src/rules/FunctionTooWideOrLarge.hs
+++ src/rules/FunctionTooWideOrLarge.hs
@@ -1,11 +1,25 @@
 {-|
 Line too long or function too long.
 -}
+
+{-# LANGUAGE ExplicitForAll #-}
+
 module FunctionTooWideOrLarge (
-  check,
 ) where
 
+import Data.Data
 import Common
+import Debug.Trace
 
+{-
 check :: ParseSuccess -> [Warn]
-check _ = []
+check m = trace (fun m) $ []
+
+fun :: ParseSuccess -> String
+fun m = fun' $ pt m
+
+fun' :: forall d. Data d => d -> String
+fun' d = show $ gmapQ check' d
+  where
+    check' :: forall d. Data d => d -> String
+    check' e = case dataTypeOf of -}
